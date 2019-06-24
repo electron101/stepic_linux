88d17bef0a28d70e8878cb260222d340db1a49f6
electron101 <sabashlive@gmail.com>
2019-06-24 11:04:04 +0300

    Закончил задачу 4.6

--- 4.6/1/solution.c
+++ 4.6/1/solution.c
@@ -1,110 +1,88 @@
+/*
+ * Некоторая утилита генерирует  довольно  большой  вывод,  а  вам  требуется  *
+ * всего-лишь посчитать количество символов '0'  в  этом  выводе.   Утилита  при
+ * запуске принимает 1 параметр.  Вам требуется разработать  программу,  которая
+ * вызывает указанную утилиту, с заданным параметром и	подсчитывает  количество
+ * символов '0' в ее выводе.  Ваша программа принимает на вход	2  параметра  --
+ * имя утилиты, в текущем каталоге и ее параметр.  Ваша программа  должна  после
+ * подсчета вывести найденное число  '0'  в  отдельной	строке,  заканчивающейся
+ * символом конца  строки.   
+ *
+ * Пример  вызова  
+ * ./solution  someprog  param  234222
+ *
+ * Представление решения
+ *
+ * Решение предоставляется в виде двух  файлов  solution.c
+ * и Makefile, в последнем предполагается цель по умолчанию, которая приводит  к
+ * сборке Вашего приложения.  Бинарный файл  вашего  решения  должен  иметь  имя
+ * solution. 
+ *
+ * Вывод
+ *
+ * Программа выводит в стандартный поток вывода число  (в  *  отдельной  строке)
+ */
+
 #include <sys/wait.h>
 #include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
-#define MAX_BUF 100
+#define COMMAND_LEN 100
+#define DATA_SIZE 4096
 
-int count_zero_in_str(char *s);
-char *readline(int fd, char * buffer); 
+int  count_zero_in_str(char *s);
 
 int main(int argc, char *argv[])
 {
-	pid_t cpid;
-	int   fd[2];
-	char  str[MAX_BUF];
-	char  cmd[20];
-	int   sum_zero = 0;
-	char  *v[3];
+	FILE *pf;
+	char command[COMMAND_LEN];
+	char data[DATA_SIZE];
+	char cmd[20];
+	int  sum_zero = 0;
+	
+	memset(cmd, '\0', COMMAND_LEN);
+	snprintf(cmd, COMMAND_LEN - 1, "%s %s", argv[1], argv[2]);
 
-	/* обнулим переменную cmd */
-	memset(cmd, '\0', 20);
-	snprintf(cmd, 19, "%s %s", argv[1], argv[2]);
-	v[0] = argv[1];
-	v[1] = argv[2];
-	v[2] = (char *)NULL;
-	
-	if (pipe(fd) == -1) {
-		perror("pipe");
-		exit(EXIT_FAILURE);
-	}
-	
-	cpid = fork();
-	if (cpid == -1) {
-		perror("fork");
-		exit(EXIT_FAILURE);
+	// Execute a process listing
+	sprintf(command, cmd); 
+
+	// Setup our pipe for reading and execute our command.
+	pf = popen(command,"r");
+
+	if(!pf) {
+		fprintf(stderr, "Could not open pipe for output.\n");
+		exit(1);
 	}
 
-	if (cpid == 0) {		/* Потомок записывает в канал */
-		/* Записываем в канал */
-		
-		close(1);	/* закроем стандартный вывод (stdout) */
-		dup2(fd[1], 1);	/* отождествляем стандарный вывод (stdout) 
-				  с файловым дискриптором канала, 
-				  предназначенным для записи */
+	// Grab data from process execution
+	while( fgets(data, DATA_SIZE , pf) ) {
+		// Print grabbed data to the screen.
+		/* fprintf(stdout, "%s",data); */
 
-		close(fd[1]);	/* закрыли файловый дескриптор канала, 
-				предназначенный для записи */
-		
-		close(fd[0]);	/* закрыли файловый дескриптор канала, 
-				предназначенный для чтения */
-		
-		/* выполним программу с параметрами */
-		/* execlp("/bin/sh","/bin/sh", "-c", "ps -ax", (char *)NULL); */
-		/* execvp(argv[1], argv); */
-		execvp(v[0], v);
-		/* execlp(argv[1], argv[2], (char *)NULL); */
-		exit(0);
-	} 
-	else {			/* Родитель читает из канала */	
-	
-		close(fd[1]);	/* закрыли файловый дескриптор канала, 
-				предназначенный для записи */
-		/* бесконечный цикл */	
-		/* do {			 */
-			memset(str, '\0', MAX_BUF); /* заполним строку 0-ми */
-
-			/* читаем из дискриптора чтения строку до символа \n */
-			readline(fd[0], str); 
-			
-			printf("%s", str);
-			
 		/* суммируем количество символов '0' */
-			sum_zero += count_zero_in_str(str); 
-			/* sleep(1); */
-
-		/* } while ( 1 ); */
-		printf("sum_zero = %d\n", sum_zero);
-		wait(NULL);	/* Ждем окончания потомка */
+		sum_zero += count_zero_in_str(data); 
 	}
+	
+	printf("%d\n", sum_zero);
+
+	if (pclose(pf) != 0)
+		fprintf(stderr," Error: Failed to close command stream \n");
+
 	return 0;
 }
 
-/* Читает из дискриптора в buffer пока не встретит \n 
- * возвращает указатель на строку buffer */
-char *readline(int fd, char * buffer) {
-	char c;
-	int  counter = 0;
-	while (read(fd, &c, 1) != 0) {
-		buffer[counter++] = c;
-		if (c == '\n') {
-			buffer[counter] = '\0';
-			break;
-		}
-	}
-	return buffer;
-}
-
-/* Возвращает количество символов '0' в строке s */
-int count_zero_in_str(char *s) 
+/* Возвращает количество нулей (символов 0) в строке.
+ * Читаем строку посимвольно, проверяя каждый символ на равенство 0
+ */
+int count_zero_in_str(char *s)
 {
 	int i;
 	int count_zero = 0;
 
-	for (i = 0; i < strlen(s); i++) {
-		if (s[i] = '0')
-			count_zero++;	
-	}
+	for(i = 0; i < strlen(s); ++i) 
+		if (s[i] == '0')
+			count_zero++;
 	return count_zero;
 }
